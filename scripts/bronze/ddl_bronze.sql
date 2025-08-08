2b198f879fbc0115e9db85f127c4c0993b0376b8
venugopalyaramasa <yaramasavenugopal@gmail.com>
2025-08-07 20:51:57 -0400

    create ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,7 +7,7 @@ Script Purpose:
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
-*/
+*//
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
